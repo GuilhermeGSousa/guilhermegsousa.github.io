429533388ffa6198e33f218374abb6e1f634b7f0
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2017-06-26 14:49:43 +0200

    Setting up AI

--- js/ai.js
+++ js/ai.js
@@ -8,9 +8,45 @@ var accessToken = "4010c7ed8915432297a2004b5ba9b30a",
   messageInternalError = "Oh no, there has been an internal server error",
   messageSorry = "I'm sorry, I don't have the answer to that yet.";
 
-textInput = document.getElementById("input");   
-
+document.getElementById("input").addEventListener("keyup", function(event){
+    if(event.keyCode == 13){
+        onClick();
+    }
+});
 function onClick(){
-    document.getElementById('output').innerHTML = "Hey";
-    
+    var textInput = document.getElementById("input");
+    if(textInput.value != ""){
+        document.getElementById("response").innerHTML = document.getElementById("response").innerHTML + textInput.value + "\n";
+        textInput.value = "";
+        document.getElementById("response").scrollTop = document.getElementById("response").scrollHeight;
+        send();
+    }
+}
+
+function send() {
+    var text = $("#input").val();
+    $.ajax({
+        type: "POST",
+        url: baseUrl + "query?v=20150910",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        headers: {
+            "Authorization": "Bearer " + accessToken
+        },
+        data: JSON.stringify({ query: text, lang: "en", sessionId: "somerandomthing" }),
+
+        success: function(data) {
+            var answer = JSON.stringify(data.result.fulfillment.speech, undefined, 2);
+            answer = answer.slice(1,answer.length-2);
+            setResponse(answer);
+        },
+        error: function() {
+            setResponse("Internal Server Error");
+        }
+    });
+    setResponse("Loading...");
+}
+
+function setResponse(val) {
+    $("#response").text(val);
 }
